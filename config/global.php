3619f82cfc61d218a8266ed67adb628413ee770c
xtianec <espino.cristhian@gmail.com>
2025-07-04 07:12:25 -0500

    Fix APP_URL detection

--- config/global.php
+++ config/global.php
@@ -19,8 +19,10 @@ define("DB_ENCODE", "utf8");
 // Nombre del proyecto
 define("PRO_NOMBRE", "PACIFIC COMPRESSOR");
 // URL base de la aplicación
-// Detectar automáticamente la URL base de la aplicación
-$basePath = rtrim(dirname($_SERVER['SCRIPT_NAME'] ?? ''), '/');
+// Detectar automáticamente la URL base sin importar desde dónde se incluya este archivo
+$scriptPath = dirname($_SERVER['SCRIPT_NAME'] ?? '');
+$basePath = preg_replace('#/vistas(/.*)?$#', '', $scriptPath); // elimina /vistas si se accede directamente
+$basePath = rtrim($basePath, '/');
 define('APP_URL', $basePath ? $basePath . '/' : '/');
 
 
